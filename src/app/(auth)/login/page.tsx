2b06e225231f60a07ef2ff218b25c047cadf9ebb
raji <kingraj1344@gmail.com>
2024-09-29 10:15:34 +0100

    s

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -14,7 +14,7 @@ import { Label } from "@/components/ui/label"
 export const description =
     "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account."
 
-export function LoginForm() {
+export default function LoginForm() {
     return (
         <Card className="mx-auto max-w-sm">
             <CardHeader>
