530bbc940c83b482033884287d914f8ea6ced17a
raji <kingraj1344@gmail.com>
2024-09-29 10:16:50 +0100

    z

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,8 +11,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export const description =
-    "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account."
+// export const description =
+//     "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account."
 
 export default function LoginForm() {
     return (
